e8e7633154176419f068d15e68ebc3c0ad179ead
vadoliyavivek <vadoliyavivek6897@gmail.com>
2024-02-22 22:37:38 +0530

    vivek
    first commit

---
+++ asp/Controllers/Feedback.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace asp.Controllers
+{
+    public class Feedback : Controller
+    {
+        public IActionResult feedback()
+        {
+            return View();
+        }
+    }
+}
---
+++ asp/Controllers/Profile.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace asp.Controllers
+{
+    public class Profile : Controller
+    {
+        public IActionResult profile()
+        {
+            return View();
+        }
+
+        public IActionResult Edit_profile()
+        {
+            return View();
+        }
+    }
+}
